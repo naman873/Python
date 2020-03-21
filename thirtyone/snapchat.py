a57f2c3c62764b91910885a8f9ba2afe1bcab69f
naman873 <namandhiman98@gmail.com>
2020-03-21 23:10:54 +0530

    push

--- thirtyone/snapchat.py
+++ thirtyone/snapchat.py
@@ -1,18 +1,38 @@
 import cv2
 
-cap=cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 classifier=cv2.CascadeClassifier("haarcascade_eye.xml")
+classifier1=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+glass=cv2.imread("glass.png")
 
 while True:
-    revel,image=cap.read()
 
-    if revel:
-        faces = classifier.detectMultiScale(image)
+    revtal, image = cap.read()
+
+
+    if revtal:
+        faces=classifier1.detectMultiScale(image)
 
         for face in faces:
-            x, y, w, h = face
-            #cv2.rectangle(image,(x,y),(x+w,y+h),color=(0,255,0),thickness=10)
-            cv2.circle(image, center=(x+20 , y+20), radius=5, color=(0, 255, 0), thickness=10)
+            x,y,w,h=face
+            #eye=cv2.rectangle(image,(x,y),(x+w,y+h),color=(0,255,0),thickness=10)
+            cut=image[y:y+h,x:x+w]
+
+            eyes=classifier.detectMultiScale(image)
+            for eye in eyes:
+                ex , ey , ew ,eh=eye
+                glases=cv2.resize(glass,width=ew)
+
+                gw , gh , gc=glases
+
+                for i in range(0,gw): 
+                    for j in range(0,gh):
+                        if glases[i,j][3] !=0:
+                            cut[ey + i,ex+j]=glases[i,j]
+
+
+
+
 
         cv2.imshow("my image", image)
 
@@ -25,3 +45,10 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
+
+
+import tensorflow
+
+
+
---
+++ thirtytwo/os working.py
@@ -0,0 +1,6 @@
+import os
+
+os.makedirs("faces.npy")
+
+    #os.makedirs("hello/mario")
+#os.mkdir("hell/bell")  ## it will add folder in particualr dir
